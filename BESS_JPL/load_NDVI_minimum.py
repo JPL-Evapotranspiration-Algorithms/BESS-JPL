989c00913df29f2ab610b64b62d95ebc3f7e5ed3
Gregory Halverson <gregory.halverson@gmail.com>
2025-12-29 15:53:57 -0800

    cleaning up static inputs

--- BESS_JPL/load_NDVI_minimum.py
+++ BESS_JPL/load_NDVI_minimum.py
@@ -10,6 +10,8 @@ from .colors import NDVI_COLORMAP_ABSOLUTE
 def load_NDVI_minimum(geometry: RasterGeometry = None, resampling: str = "nearest") -> Raster:
     filename = join(abspath(dirname(__file__)), "NDVI_minimum.tif")
     image = Raster.open(filename, geometry=geometry, resampling=resampling, nodata=np.nan)
-    image.cmap = NDVI_COLORMAP_ABSOLUTE
+    
+    if isinstance(image, Raster):
+        image.cmap = NDVI_COLORMAP_ABSOLUTE
 
     return image
